35b721a700d3a73f41913bcbe636034181682ef0
rutthy52rg <rutthy52.rg@gmail.com>
2022-07-29 15:37:27 +0200

    add Team class with his config

---
+++ classes/Team.js
@@ -0,0 +1,17 @@
+export default class Team{ 
+     constructor(name, config = {}){ 
+         this.name = name,
+         this.setUpConfig(config)
+    }
+
+    setUpConfig(config = {}){
+        const defaultConfig = {
+            goalsTo: 0,
+            goalsFrom: 0,
+            totalGoalsTo:0,
+            totalGoalsFrom:0
+        }
+        this.config = Object.assign(defaultConfig, config)
+    }
+}
+

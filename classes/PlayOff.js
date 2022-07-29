7ba79520f01eaffef6e80230a892531c5bf47ace
rutthy52rg <rutthy52.rg@gmail.com>
2022-07-29 20:04:53 +0200

    add method to random order of teams

--- classes/PlayOff.js
+++ classes/PlayOff.js
@@ -99,6 +99,25 @@ export default class PlayOff {
         }
         return arrResult;
     }
+    /**
+   * method to random order of teams
+   * @param {teams} from this.teams
+   */
+    shuffle(array) {
+        let currentIndex = array.length,
+            randomIndex;
+        // While there remain elements to shuffle...
+        while (currentIndex != 0) {
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+            // And swap it with the current element.
+            [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex],
+                array[currentIndex],
+            ];
+        }
+        return array;
+    }
 
-    
 }
